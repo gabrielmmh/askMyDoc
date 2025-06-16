0df1ec0947e1c0fa37faa0ebbeefd5f3e5d71319
Gabriel Hermida <gabrielhermida948@gmail.com>
2025-06-15 21:37:55 -0300

    chore(frontend): ajustes finais em estilos e configurações do Next e Tailwind

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
     return config;
